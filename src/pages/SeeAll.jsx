85a9b09fe0751081ad98c1d76ca23005eb9ed028
Md Rayyan Sarfaraz <mdrayyansarfaraz@gmail.com>
2024-12-24 10:56:50 +0530

    added a loder

--- src/pages/SeeAll.jsx
+++ src/pages/SeeAll.jsx
@@ -1,10 +1,12 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 import Card2 from '../components/Card2';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 function SeeAll() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false); 
   const [searchParams] = useSearchParams(); 
 
   useEffect(() => {
@@ -12,11 +14,14 @@ function SeeAll() {
 
     const fetchData = async () => {
       try {
+       
+        setLoading(true); 
         const response = await axios.get(`http://localhost:8080/${category}`);
-        setData(response.data); 
-        console.log(response.data);
+        setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false); 
       }
     };
 
@@ -26,16 +31,25 @@ function SeeAll() {
   }, [searchParams]);
 
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 p-4">
-      {data.map((item, index) => (
-        <Card2
-          key={index}
-          title={item.destination}
-          price={item.price}
-          location={item.location}
-          url={item.url}
-        />
-      ))}
+    <div className="p-4">
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          
+          <ClipLoader color="#3498db" size={50} /> 
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3">
+          {data.map((item, index) => (
+            <Card2
+              key={index}
+              title={item.destination}
+              price={item.price}
+              location={item.location}
+              url={item.url}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
